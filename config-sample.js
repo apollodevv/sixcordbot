c60c7fefbd240453481aaed70acea0868df9b983
apollo <mxrcdev@gmail.com>
2021-03-24 11:01:26 +0000

    Create config-sample.js

---
+++ config-sample.js
@@ -0,0 +1,9 @@
+module.exports = {
+  "prefix": "s?",
+  "token": "BOT-TOKEN",
+  "ownerID": "ID",
+  "web": {
+     "website": "https://sixcord.xyz/",
+     "dashboard": "https://dash.sixcord.xyz/",
+    },
+};
